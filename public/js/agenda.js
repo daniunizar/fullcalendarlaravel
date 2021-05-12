60491f45d7ba9171355c9af96cc34db7d9dbabbd
Dani <daniunizar@gmail.com>
2021-05-12 10:56:26 +0200

    Primer commit: generado sistema de autenticación y se muestra calendario

---
+++ public/js/agenda.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('agenda');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      locale:"es",
+      headerToolbar: {
+        left: 'prev,next today myCustomButton',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      }
+    });
+    calendar.render();
+  });
---
+++ resources/views/event/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div id="agenda">
+        Aquí iría el calendario.
+    </div>
+</div>
+@endsection
