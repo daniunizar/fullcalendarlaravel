391186dd92470573a75a32d9d26da7de0761a64e
Dani <daniunizar@gmail.com>
2021-05-19 10:42:16 +0200

    Cambiara redirección tras loguear y registrar a event (ALTER_HOME) en vez de a HOME. Ahora tras loguearte o registrarte te lleva al calendario.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
-
+    public const ALTER_HOME = '/event';
     /**
      * The controller namespace for the application.
      *
